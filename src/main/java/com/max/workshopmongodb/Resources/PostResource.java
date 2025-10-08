909350fc6016a3e95c88a377fb732c7e09212285
Max-leal <Max13leal@gmail.com>
2025-10-08 15:47:43 -0300

    Obtendo post por id

---
+++ src/main/java/com/max/workshopmongodb/Resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.max.workshopmongodb.resources;
+
+import com.max.workshopmongodb.domain.Post;
+import com.max.workshopmongodb.domain.User;
+import com.max.workshopmongodb.dto.UserDTO;
+import com.max.workshopmongodb.services.PostService;
+import com.max.workshopmongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource { // enves de resource poderia ser controller
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/max/workshopmongodb/services/PostService.java
@@ -0,0 +1,27 @@
+package com.max.workshopmongodb.services;
+
+import com.max.workshopmongodb.domain.Post;
+import com.max.workshopmongodb.domain.User;
+import com.max.workshopmongodb.dto.UserDTO;
+import com.max.workshopmongodb.repository.PostRepository;
+import com.max.workshopmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        //User user = repo.findOne(id);
+        //if (user == null) {
+        // throw new ObjectNotFoundException("User not found");
+        // }
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("User not found: " + id));
+    }
+}
